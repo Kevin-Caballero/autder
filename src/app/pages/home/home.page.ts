c55ebe47d901bb46e690c28e95ec5ccec4bc4fba
Kevin <caballero.kevin94@gmail.com>
2023-02-12 09:54:45 +0100

    wip: home stile

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,6 +6,7 @@ import { ILaunch } from 'src/app/models/launch.interface';
 import { IPeopleInSpacePerson } from 'src/app/models/people-in-space-person.interface';
 import { IResponsePeopleInSpace } from 'src/app/models/people-in-space-response.interface';
 import { IResponse } from 'src/app/models/response.interface';
+import { IStatus } from 'src/app/models/status.interface';
 
 @Component({
   selector: 'app-folder',
@@ -50,7 +51,7 @@ export class HomePage implements OnInit {
         this.nextLaunches = res.results;
         console.log(this.nextLaunches);
         this.loading = false;
-      }, 3000);
+      }, 1000);
     });
   }
 
@@ -70,5 +71,33 @@ export class HomePage implements OnInit {
 
   getAliensInSpace() {
     this.aliensInSpace = Math.floor(Math.random() * 10) + 1;
+  }
+
+  getStatusColor(status: IStatus | undefined): { text: string, bgColor: string, textColor: string } {
+    let textColor: string = 'var(--ion-color-primary)';
+    let bgColor: string = 'var(--ion-color-tertiary)';
+    let text: string = '';
+    if (status)
+      switch (status.name.toLowerCase()) {
+        case 'to be determined':
+        case 'to be confirmed':
+          bgColor = 'var(--ion-color-warning-shade)';
+          text = 'undefined';
+          textColor = 'var(--ion-color-warning-contrast)';
+          break;
+        case 'launch successful':
+          bgColor = 'var(--ion-color-success-shade)';
+          text = 'successful';
+          textColor = 'var(--ion-color-success-contrast)';
+          break;
+        case 'go for launch':
+          bgColor = 'var(--ion-color-success-shade)';
+          text = 'ready';
+          textColor = 'var(--ion-color-success-contrast)';
+          break;
+        default:
+          break;
+      }
+    return { text, bgColor, textColor };
   }
 }
